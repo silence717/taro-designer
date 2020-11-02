d509436ba727abb819adc201cbf96340ee6e74c6
fang.yang <yangfang2028@126.com>
2020-11-02 17:47:47 +0800

    ♻️  Button 组件底层更改为 tarojs/components

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'cloud-react';
+import { Button } from '@tarojs/components/dist-h5/react';
 
 export default class Button1 extends Component {
-
 	render() {
-		const { text, size, theme, ...othersProps } = this.props;
-		return <Button type={theme} size={size} {...othersProps}>{text}</Button>
+		const { text, ...othersProps } = this.props;
+		return <Button {...othersProps}>{text}</Button>;
 	}
 }
 
 Button1.propTypes = {
 	text: PropTypes.string,
 	size: PropTypes.string,
-	theme: PropTypes.string,
+	type: PropTypes.string
 };
 
 Button1.defaultProps = {
 	text: 'ok',
 	size: 'default',
-	theme: 'normal'
+	type: 'default'
 };
