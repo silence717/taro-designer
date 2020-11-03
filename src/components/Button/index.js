165dba884b81c2932c589617fe759eb80ebc88c7
fang.yang <yangfang2028@126.com>
2020-11-03 18:51:55 +0800

    ♻️ 完善button组件的可配置信息

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@tarojs/components/dist-h5/react';
+import { noop } from '@utils';
 
 export default class Button1 extends Component {
 	render() {
@@ -12,11 +13,25 @@ export default class Button1 extends Component {
 Button1.propTypes = {
 	text: PropTypes.string,
 	size: PropTypes.string,
-	type: PropTypes.string
+	type: PropTypes.string,
+	hoverClass: PropTypes.string,
+	plain: PropTypes.bool,
+	disabled: PropTypes.bool,
+	loading: PropTypes.bool,
+	hoverStartTime: PropTypes.number,
+	hoverStayTime: PropTypes.number,
+	onClick: PropTypes.func
 };
 
 Button1.defaultProps = {
 	text: 'ok',
 	size: 'default',
-	type: 'default'
+	type: 'default',
+	hoverClass: 'none',
+	plain: false,
+	disabled: false,
+	loading: false,
+	hoverStartTime: 20,
+	hoverStayTime: 70,
+	onClick: noop
 };
